7df56b270b75bbfcf176929ce4b2cc2cd781d2ca
priyanshu rai <priyanshu.rai@teqexpert.com>
2023-12-22 12:45:06 +0530

    webwhr

--- handlers/users/userAuthentication/resetPassword.go
+++ handlers/users/userAuthentication/resetPassword.go
@@ -2,7 +2,7 @@ package userAuthenticate
 
 import (
 	"opsy_backend/database"
-	userAuth "opsy_backend/dto"
+	userAuth "opsy_backend/dto/users/userAuthentication"
 	"opsy_backend/entity"
 	"opsy_backend/utils"
 
